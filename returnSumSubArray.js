579a0e6b9635739f637c76f79917f4ad80975cb4
Rishabh Anand <ranand16@gmail.com>
2020-07-29 09:30:36 +0530

    updated the algo for returnsumsubarray

--- returnSumSubArray.js
+++ returnSumSubArray.js
@@ -1,20 +1,21 @@
+// kadane's algorithm used here
 function returnSumSubArray(arr, reqsum){
-    console.log(" ------------------------------------------------------------- ")
-    console.log(arr, reqsum)
+    // console.log(" ------------------------------------------------------------- ")
+    // console.log(arr, reqsum)
     if(arr.length<=0) return []
     let l_pointer = 0, r_pointer = 0, currSum = 0;
-    while(l_pointer<arr.length && r_pointer<arr.length){
-        console.log(l_pointer + " --- " + r_pointer + " --- " + currSum + " --- " + reqsum)
+    while(l_pointer<=arr.length && r_pointer<=arr.length){
+        // console.log("START ------ " + l_pointer + " --- " + r_pointer + " --- " + currSum + " --- " + reqsum)
         if(currSum < reqsum){
             currSum = currSum + arr[r_pointer];
             r_pointer++;
         } else if(currSum > reqsum){
             currSum = currSum - arr[l_pointer];
             l_pointer++;
-        } else if(reqsum == currSum){
+        } else if(reqsum === currSum){
             return arr.slice(l_pointer, r_pointer)
         }
-        console.log(l_pointer + " --- " + r_pointer)
+        // console.log("END   ------ " + l_pointer + " --- " + r_pointer + " --- " + currSum + " --- " + reqsum)
     }
     return []
 }
