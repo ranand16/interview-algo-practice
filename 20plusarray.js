386b7afe0cadacabb54832ea31d70bc291980d7d
RIshabh Anand <rishabh@zvky.com>
2020-08-04 07:46:57 +0530

    Question 2 and 3 from 20plus question set

--- 20plusarray.js
+++ 20plusarray.js
@@ -8,12 +8,32 @@ function missing1ton(arr){
     }
 }
 
-//Question 1 
+//Question 1 second method
 function missing1tonSecondWay(arr){
     if(arr.length<=0) return null
     let origSumArr = ((arr.length+2)*(arr.length+1))/2
     let arrSum = 0;
     for(let i=0; i<arr.length; i++) arrSum = arrSum + arr[i];
-    // console.log(origSumArr, " --- ", arrSum);
     return origSumArr - arrSum;
+}
+
+// Question 2
+function findDup(arr){
+    if(arr.length<=0) return []
+    let content = new Map();
+    let dup = new Array();
+    for(let i=0; i<arr.length; i++) {
+        if(content.has(arr[i]) && content.get(arr[i])===1) { dup.push(arr[i]); content.set(arr[i], content.get(arr[i])+1) }
+        else if(content.has(arr[i])) content.set(arr[i], content.get(arr[i])+1);
+        else if(!content.has(arr[i])) content.set(arr[i], 1);
+    }
+    return dup
+}
+
+// Question 3
+function smallestLargest(arr){
+    console.log(arr);
+    if(!arr.length) return [null, null]
+    arr.sort((a,b)=>a-b)
+    return [arr[0], arr[arr.length-1]]
 }
